6a0054318895a219bee0c52787db948e9f9e8f7b
ydw1996 <112791174+ydw1996@users.noreply.github.com>
2022-10-17 19:49:25 +0900

    1017

--- js/index.js
+++ js/index.js
@@ -36,60 +36,83 @@
 //     });
 //   });
 // });
-
-
+var obj = $('.main__logo img');
   window.onload = () => {
-  gsap.to(".main__logo", {
-    opacity: 1,
-    duration: 0.55,
-    brightness: 2,
-    delay: 0.55,
+  gsap.fromTo(".main__logo img",{y: 1500, transform: 'scale(2.5)',}, {
+    duration: 1,
+    delay: 0.2,
+    ease: "circ.out",
+    y: 0,
+    transform: 'scale(1)',
+  })
+  gsap.to(".main__logo img", {
+    duration: 0.5,
+    filter: 'brightness(3)',
+    ease: "circ.out",
+    delay: 1.02,
+  })
+  gsap.to(".main__logo img", {
+    duration: 0.8,
+    filter: 'brightness(1)',
+    ease: "circ.out",
+    delay: 1.52,
   })  
   gsap.to(".st05", {
     opacity: 1,
     duration: 0.08,
-    delay: 0.08,
+    delay: 0.7,
   })
   gsap.to(".st04", {
     opacity: 1,
     duration: 0.08,
-    delay: 0.16,
+    delay: 0.78,
   })
   gsap.to(".st03", {
     opacity: 1,
     duration: 0.08,
-    delay: 0.24,
+    delay: 0.86,
   })
   gsap.to(".st02", {
     opacity: 1,
     duration: 0.08,
-    delay: 0.32,
+    delay: 0.94,
   })
   gsap.to(".st01", {
     opacity: 1,
     duration: 0.08,
-    delay: 0.4,
+    delay: 1.02,
   })
-
   gsap.to(".st05", {
     opacity: 0,
     duration: 0.08,
-    delay: 0.55,
+    delay: 1.1,
   })
   gsap.to(".st04", {
     opacity: 0,
     duration: 0.08,
-    delay: 0.63,
+    delay: 1.18,
   })
   gsap.to(".st03", {
     opacity: 0,
     duration: 0.08,
-    delay: 0.71,
+    delay: 1.26,
   })
   gsap.to(".st02", {
     opacity: 0,
     duration: 0.08,
-    delay: 0.79,
+    delay: 1.34,
   })
-
+  gsap.to("header", {
+    opacity: 1,
+    duration: 1,
+    ease: "circ.out",
+    delay: 0.8,
+  })
+  gsap.to(".header__logo__box ", {
+    duration: 1,
+    rotation: 360,
+    ease: "circ.out",
+    delay: 0.8,
+  })
 }
+
